7345c52453ef7d943672acda9f8d178f11e16f01
algermo <algermo@mail.gvsu.edu>
2016-12-29 11:00:21 -0500

    Initial commit of Android project

---
+++ HonorsProject/app/src/main/java/algermo/honorsproject/HomePage.java
@@ -0,0 +1,13 @@
+package algermo.honorsproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
