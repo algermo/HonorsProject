fc9ea7c0f5100007248cf6067579a66dda55c849
algermo <algermo@mail.gvsu.edu>
2017-02-17 14:44:20 -0500

    I made a toast

--- HonorsProject/app/src/main/java/algermo/honorsproject/HomePage.java
+++ HonorsProject/app/src/main/java/algermo/honorsproject/HomePage.java
@@ -2,6 +2,10 @@ package algermo.honorsproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+import android.content.Context;
 
 public class HomePage extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        Button btn1 = (Button) findViewById(R.id.button);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Hello toast!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
     }
 }
