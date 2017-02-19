541b1e95b94120b201d04541369abc694a7db049
Jennifer Moon <moonje@mail.gvsu.edu>
2017-02-19 16:10:07 -0500

    Added comments to clarify methods

--- HonorsProject/app/src/main/java/algermo/honorsproject/GameLogic.java
+++ HonorsProject/app/src/main/java/algermo/honorsproject/GameLogic.java
@@ -1,73 +1,127 @@
 package algermo.honorsproject;
 import java.util.Arrays;
+import java.util.ArrayList;
 
-/**
- * Created by Owner on 2/19/2017.
- */
+/***************************************************************************************************
+ * Jennifer Moon & Molly Alger
+ *
+ * HON499 Senior Project
+ **************************************************************************************************/
 
 public class GameLogic {
 
+    /* Number of ROWS in the board*/
     private int rows;
+
+    /* Number of COLUMNS in the board*/
     private int columns;
+
+    /* The BOARD */
     private int[][] board;
 
-//    public static void main(String args[]){
-//        GameLogic example = new GameLogic();
-//        example.place(0, 0);
-//        example.printBoard();
-//
-//        if (example.check() == true){
-//            System.out.println("true");
-//        } else {
-//            System.out.println("false");
-//        }
-//
-//        example.place(2,0);
-//        example.printBoard();
-//        if (example.check() == true){
-//            System.out.println("true");
-//        } else {
-//            System.out.println("false");
-//        }
-//    }
+    /* The rook POLYNOMIAL for the corresponding board*/
+    private int[] poly;
 
+    /* The REMAINING number of ways to place 'x' rooks*/
+    private int[] remaining;
+
+    /* All ANSWERS the user has submitted*/
+    private ArrayList<int[][]> ans;
+
+
+    /***********************************************************************************************
+     * Constructor used to create a blank board with c columns and r rows
+     *
+     * @param c the number of columns
+     * @param r the number of rows
+     **********************************************************************************************/
     public GameLogic (int c, int r) {
         rows = r;
         columns = c;
         board = new int[c][r];
     }
 
-    public GameLogic (int[][] brd){
+    /***********************************************************************************************
+     * Constructor used to create a given board with its corresponding rook polynomial
+     *
+     * @param brd
+     * @param polynomial
+     **********************************************************************************************/
+    public GameLogic (int[][] brd, int[] polynomial){
         board = brd;
         columns = brd.length;
         rows = brd[0].length;
+
+        poly = polynomial;
+        remaining = polynomial;
+
+        ans = new ArrayList<int[][]>();
     }
 
-//    public GameLogic (){
-//        //used only for testing thus far
-//        rows = 4;
-//        columns = 4;
-//        board = new int[columns][rows];
-//        board[1][0] = -1;
-//        board[2][1] = -1;
-//        board[1][2] = -1;
-//        board[2][2] = -1;
-//        board[3][2] = -1;
-//        board[1][3] = -1;
-//
-//        System.out.println(Arrays.deepToString(board));
-//    }
+    /***********************************************************************************************
+     * Returns the number of ways remaining to place each number of rooks
+     *
+     * @return a string with the number of ways to place 'x' rooks remaining
+     **********************************************************************************************/
+    public String getRemaining(){
+        String msg = "You have this many remaining to place:\n" + poly[0] +
+                " ways to place 0";
 
-//    public void printBoard(){
-//        System.out.println(Arrays.deepToString(board));
-//    }
+        for (int i = 1; i < remaining.length; i++){
+            msg = msg + "\n" + remaining[i] + " ways to place " + i;
+        }
 
-//    public void restrict(int[][] restrictions){
-//        for (int i = 0; i < restrictions[0].length; i++){
-//            board[restrictions[0][i]][restrictions[1][i]] = -1;
-//        }
-//    }
+        return msg;
+    }
 
+    /***********************************************************************************************
+     * Checks to see if the answer is a distinct answer, and records the given answer if it is
+     * distinct. Removes one from the respective rook number of remaining rooks to place.
+     *
+     * @param brd
+     * @return whether or not the user has already submitted given answer
+     **********************************************************************************************/
+    public boolean answer(int[][] brd){
+        if (!ans.contains(brd)){
+            ans.add(brd);
+
+            int rooks = 0;
+
+            for (int i = 0; i < brd.length; i++){
+                for (int j = 0; j < brd.length; j++){
+                    if (brd[i][j] == 1){
+                        rooks++;
+                    }
+                }
+            }
+
+            remaining[rooks]--;
+            return true;
+        }
+        return false;
+    }
+
+    /***********************************************************************************************
+     * Returns the rook polynomial of the given starting board
+     *
+     * @return the rook polynomial of the board
+     **********************************************************************************************/
+    public String getPolynomial(){
+        String p = "" + poly[0];
+
+        for (int i = 1; i < poly.length; i++){
+            p = p + " + " + poly[i] + "x^" + i;
+        }
+
+        return p;
+    }
+
+    /***********************************************************************************************
+     * Places (or removes) a rook in the given position
+     *
+     * @param c the column the rook is to be placed in
+     * @param r the row the rook is to be placed in
+     **********************************************************************************************/
     public void place(int c, int r){
         if (board[c][r] == 0){
             board[c][r] = 1;
@@ -76,9 +130,13 @@ public class GameLogic {
             //} if (board[r][c] = -1) {
             //do nothing since you can't place here
         }
-
     }
 
+    /***********************************************************************************************
+     * Checks to see if the given rook configuration is nonattacking or attacking
+     *
+     * @return whether or not the rook configuration is attacking
+     **********************************************************************************************/
     public boolean check(){
         for (int i = 0; i < columns; i++){
             int sum = 0;
@@ -106,5 +164,4 @@ public class GameLogic {
 
         return true;
     }
-
 }
