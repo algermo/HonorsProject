79008a4a4a1f8edeb76fd3613154b38cfb9f54dc
algermo <algermo@mail.gvsu.edu>
2017-02-23 22:30:46 -0500

    3x3 loading, random board working

--- HonorsProject/app/src/main/java/algermo/honorsproject/ThreeByThreeGame.java
+++ HonorsProject/app/src/main/java/algermo/honorsproject/ThreeByThreeGame.java
@@ -8,9 +8,19 @@ import android.view.MotionEvent;
 import android.graphics.Color;
 import android.view.LayoutInflater;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Random;
+
+
 public class ThreeByThreeGame extends AppCompatActivity {
 
     GameLogic game;
+    public static ArrayList<int[][]> allBoards;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,59 +28,111 @@ public class ThreeByThreeGame extends AppCompatActivity {
         setContentView(R.layout.activity_three_board);
         View root = LayoutInflater.from(this).inflate(R.layout.activity_three_board, null);
         setContentView(root);
+        allBoards = new ArrayList<int[][]>();
+        readFile();
+
+        Random rand = new Random();
 
         int[][] board = new int[3][3];
-        board[1][0] = -1;
-        board[2][1] = -1;
-        board[1][2] = -1;
-        board[2][2] = -1;
+        board = allBoards.get(rand.nextInt(allBoards.size()));
 
-        game = new GameLogic(board);
+        //game = new GameLogic(board);
 
-        Button[][] buttons = new Button[3][3];
+        final Button[][] buttons = new Button[3][3];
 
         final Button btn0 = (Button) findViewById(R.id.button0);
-        btn0.setTag("0");
+        buttons[0][0] = btn0;
         final Button btn1 = (Button) findViewById(R.id.button1);
-        btn0.setTag("1");
+        buttons[0][1] = btn1;
         final Button btn2 = (Button) findViewById(R.id.button2);
-        btn0.setTag("2");
+        buttons[0][2] = btn2;
         final Button btn3 = (Button) findViewById(R.id.button3);
-        btn0.setTag("3");
+        buttons[1][0] = btn3;
         final Button btn4 = (Button) findViewById(R.id.button4);
-        btn0.setTag("4");
+        buttons[1][1] = btn4;
         final Button btn5 = (Button) findViewById(R.id.button5);
-        btn0.setTag("5");
+        buttons[1][2] = btn5;
         final Button btn6 = (Button) findViewById(R.id.button6);
-        btn0.setTag("6");
+        buttons[2][0] = btn6;
         final Button btn7 = (Button) findViewById(R.id.button7);
-        btn0.setTag("7");
+        buttons[2][1] = btn7;
         final Button btn8 = (Button) findViewById(R.id.button8);
-        btn0.setTag("8");
+        buttons[2][2] = btn8;
 
-        int count = 0;
         for(int i = 0; i < 3; i++) {
             for(int j = 0; j < 3; j++) {
-                buttons[i][j] = (Button) root.findViewWithTag(Integer.toString(count));
-
-                /*if(board[i][j] == -1) {
-                    buttons[i][j].setBackgroundColor(Color.RED);
+                if(board[i][j] == 1) {
                     buttons[i][j].setEnabled(false);
-                }*/
+                    buttons[i][j].setBackgroundResource(R.drawable.fire);
+                }
+                final int x = i;
+                final int y = j;
+                buttons[i][j].setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View view, MotionEvent event) {
+                        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                            buttons[x][y].setEnabled(false);
+                            buttons[x][y].setBackgroundColor(Color.BLUE);
+                        }
+                        return false;
+                    }
+
+                });
+            }
+        }
+
+
+    }
+
+    public void readFile() {
+
+        //read the file
+        InputStream inStream = getApplicationContext().getResources().openRawResource(R.raw.x3);
+        InputStreamReader inputreader = new InputStreamReader(inStream);
+        BufferedReader buffreader = new BufferedReader(inputreader);
+        String line;
+        StringBuilder text = new StringBuilder();
+
+        try {
+
+            try {
+                while ((line = buffreader.readLine()) != null) {
+
+                    String[] part = line.split(" ");
+                    int size = Integer.parseInt(part[0]);
+                    int[] boardVals = new int[size * size];
+
+                    for (int i = 1; i < (boardVals.length); i++) {
+                        boardVals[i - 1] = Integer.parseInt(part[i]);
+                    }
+
+                    createBoard(size, boardVals);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            inStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.print(allBoards);
+
+    }
+
+    public void createBoard(int size, int[] values){
+
+        int count = 0;
+        int[][] board = new int[size][size];
+
+        for(int i = 0; i <= size-1; i++) {
+            for(int j = 0; j <= size-1; j++) {
+                board[i][j] = values[count];
                 count++;
             }
         }
 
-        btn0.setOnTouchListener(new View.OnTouchListener() {
+        allBoards.add(board);
 
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                if(event.getAction() == MotionEvent.ACTION_UP) {
-                    btn0.setBackgroundColor(Color.RED);
-                }
-                return false;
-            }
-
-        });
     }
 }
