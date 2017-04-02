8d0295f9f1c8b8007be2e2f97055435dd0e08e1e
algermo <algermo@mail.gvsu.edu>
2017-04-01 21:39:00 -0400

    Layout update, etc

--- HonorsProject/app/src/main/java/algermo/honorsproject/HomePage.java
+++ HonorsProject/app/src/main/java/algermo/honorsproject/HomePage.java
@@ -1,10 +1,15 @@
 package algermo.honorsproject;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.RelativeLayout;
 
 public class HomePage extends AppCompatActivity {
 
